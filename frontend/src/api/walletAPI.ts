ee1b935d344050b62f81ff226f73d3453f714541
Albert Tian <atian@berkeley.edu>
2022-02-19 01:24:51 -0800

    content: metamask connection added back in

--- frontend/src/api/walletAPI.ts
+++ frontend/src/api/walletAPI.ts
@@ -1,5 +1,3 @@
-import { Keypair, PublicKey } from "@solana/web3.js";
-
 // Used to check if we have metamask installed. Returns
 const checkMetaConnection = async (): Promise<any> => {
   let account = null;
@@ -49,49 +47,49 @@ const connectMeta = async (): Promise<any> => {
 
 // Check if we have an existing solana account. Passive request
 // TODO: GET RID OF ANY
-const checkSolanaConnection = async (): Promise<Keypair> => {
-  let address: unknown = null;
-  try {
-    const { solana } = window;
+// const checkSolanaConnection = async (): Promise<Keypair> => {
+//   let address: unknown = null;
+//   try {
+//     const { solana } = window;
 
-    if (solana) {
-      if (solana.isPhantom) {
-        console.log("Phantom wallet found!");
+//     if (solana) {
+//       if (solana.isPhantom) {
+//         console.log("Phantom wallet found!");
 
-        /*
-         * The solana object gives us a function that will allow us to connect
-         * directly with the user's wallet!
-         */
-        const response = await solana.connect({ onlyIfTrusted: true });
-        console.log(
-          "Connected with Public Key:",
-          response.publicKey.toString()
-        );
-        address = response;
-      }
-    } else {
-      console.log("No phantom wallet");
-    }
-  } catch (error) {
-    console.error(error);
-  }
-  return address as Keypair;
-};
+//         /*
+//          * The solana object gives us a function that will allow us to connect
+//          * directly with the user's wallet!
+//          */
+//         const response = await solana.connect({ onlyIfTrusted: true });
+//         console.log(
+//           "Connected with Public Key:",
+//           response.publicKey.toString()
+//         );
+//         address = response;
+//       }
+//     } else {
+//       console.log("No phantom wallet");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+//   return address as Keypair;
+// };
 
-// Connect to the wallet. Will make an active request and prompt the user
-// TODO: GET RID OF ANY
-const connectSolana = async (): Promise<Keypair> => {
-  let address: unknown = null;
-  const { solana } = window;
+// // Connect to the wallet. Will make an active request and prompt the user
+// // TODO: GET RID OF ANY
+// const connectSolana = async (): Promise<Keypair> => {
+//   let address: unknown = null;
+//   const { solana } = window;
 
-  if (solana) {
-    // The response will be a KeyPair...?
-    const response = await solana.connect();
-    console.log("Connected with Public Key:", response.publicKey.toString());
-    // setWalletAddress(response.publicKey.toString());
-    address = response;
-  }
-  return address as Keypair;
-};
+//   if (solana) {
+//     // The response will be a KeyPair...?
+//     const response = await solana.connect();
+//     console.log("Connected with Public Key:", response.publicKey.toString());
+//     // setWalletAddress(response.publicKey.toString());
+//     address = response;
+//   }
+//   return address as Keypair;
+// };
 
-export { checkMetaConnection, connectMeta, checkSolanaConnection, connectSolana };
+export { checkMetaConnection, connectMeta };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Button, Input, useToast, Image } from "@chakra-ui/react";
 import axios from "axios";
 import Head from "next/head";
 import React, { BaseSyntheticEvent, useEffect, useState } from "react";
-// import { checkMetaConnection, connectMeta } from "../api/walletAPI";
+import { checkMetaConnection, connectMeta } from "../api/walletAPI";
 // import { getAllWaves, getWaveContract, wave } from "../api/wavePortalAPI";
 import { Container } from "../components/Container";
 
@@ -13,6 +13,9 @@ const getLink = (address: string, tokenId: number) => {
 };
 
 const Index = () => {
+  // Metamast specific
+  const [currentAccount, setCurrentAccount] = useState("");
+
   // Frontend specific
   const [inputMessage, setInputMessage] = useState("");
   const toast = useToast();
@@ -23,19 +26,31 @@ const Index = () => {
 
   const handleChange = (event: any) => setInputMessage(event.target.value);
 
-  const getAsset = () => {
-    const coin = "0x42dce91f8d7aa15d707e2598dd9dcdb114e58a21";
-    const link = getLink(inputMessage, 1);
-    console.log("HERES THE LINK:", link);
+  const onConnectWallet = async () => {
+    const account = await connectMeta();
+    if (account) {
+      setCurrentAccount(account);
+      // Update info 
+    } else {
+      toast({
+        title: "Make sure you have metamask!",
+        description: "Get the chrome extension to connect your wallet",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
 
-    const options = { method: "GET" };
+  const getAsset = () => {
+    const link = getLink(inputMessage, 1);
 
     axios
       .get(link)
       .then(function (response) {
         // handle success
         console.log(response);
-        setAsset(response.data)
+        setAsset(response.data);
       })
       .catch(function (error) {
         // handle error
@@ -46,34 +61,52 @@ const Index = () => {
       });
   };
 
+  // Run on load
+  useEffect(() => {
+    // Can use anything that needs account on startup in onLoad
+    const onLoad = async () => {
+      const account = await checkMetaConnection();
+      if (account && account !== currentAccount) {
+        setCurrentAccount(account);
+      }
+    };
+
+    onLoad();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container>
       <Head>
         <title>TREEHACKS</title>
       </Head>
-      {/* Conditionally render connect button */}
-      <>
-        <Input
-          value={inputMessage}
-          onChange={handleChange}
-          placeholder={"link here"}
-        />
-        <Button
-          width={"100%"}
-          // type="submit"
-          isLoading={isMining}
-          loadingText={"mining ..."}
-          onClick={() => getAsset()}
-        >
-          👋 gimme an address to monke
-        </Button>
-      </>
-      {asset && 
-      <>
-        heres da pic
-        <Image src={asset.image_original_url}/>
-      </>
-      }
+      {/* Only render connect button if not connected */}
+      {currentAccount ? (
+        <>
+          <Input
+            value={inputMessage}
+            onChange={handleChange}
+            placeholder={"link here"}
+          />
+          <Button
+            width={"100%"}
+            // type="submit"
+            isLoading={isMining}
+            loadingText={"mining ..."}
+            onClick={() => getAsset()}
+          >
+            👋 gimme an address to monke
+          </Button>
+          {asset && (
+            <>
+              heres da pic
+              <Image src={asset.image_original_url} />
+            </>
+          )}
+        </>
+      ) : (
+        <Button onClick={() => onConnectWallet()}>CONNECT 2 METAMASK</Button>
+      )}
     </Container>
   );
 };
