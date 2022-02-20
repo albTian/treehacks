86f24f228b2905c6bd719c693aecd6288e894426
Albert Tian <atian@berkeley.edu>
2022-02-20 00:28:19 -0800

    misc: another go

--- heroku-img/server/index.js
+++ heroku-img/server/index.js
@@ -10,11 +10,20 @@ s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 const { PythonShell } = require('python-shell');
 
 
-// I HAVE DEFATED CORS
+// CORS IS FUCKING AIDS
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Origin", "https://treehacks.vercel.app"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "https://treehacks.vercel.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://treehacks.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     next();
 });
 app.use(express.json());
